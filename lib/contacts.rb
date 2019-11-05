479b1f9161df73e2cde025b18c984252d155c1c1
Michael Calkins <m.calkins576@gmail.com>
2019-11-05 17:26:33 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,14 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |name, info|
+    if name == "Freddy Mercury"
+      info.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+      end
+    end
+  end
+  
 end
